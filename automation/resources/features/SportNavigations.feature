8865762199e243d84e2b101b35a3724c7cc15b43
Sreekanth <kanth.new@gmail.com>
2015-10-09 20:08:00 +0100

    Fixed stepdefintions Asserts

--- automation/resources/features/SportNavigations.feature
+++ automation/resources/features/SportNavigations.feature
@@ -3,9 +3,14 @@ Feature: Navigation to different Sports
 Background: Browser is openned and Navigate to Homepage
 Given browser is open with Home Page
 
-Scenario:
+Scenario: Test 1
 Then Sport Types are displayed
 
-Scenario:
+Scenario: Test 2
 When user click on a sport "Football"
 Then Sports page "Football" is displayed 
+
+
+Scenario: Test 3
+When user click on a sport "Football"
+Then Sports page "Football" is displayed
--- automation/src/test/java/com/bdd/automation/stepdefinitions/HomePageStepDefinitions.java
+++ automation/src/test/java/com/bdd/automation/stepdefinitions/HomePageStepDefinitions.java
@@ -1,6 +1,9 @@
 package com.bdd.automation.stepdefinitions;
 
 
+
+import org.junit.Assert;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -13,18 +16,21 @@ public class HomePageStepDefinitions {
 public void browser_is_open_with_Home_Page() throws Throwable {
     // Write code here that turns the phrase above into concrete actions
     System.out.println("browser is open with Home Page");
+    //Assert.assertTrue("Failed", true);
 }
 
 @Then("^Sport Types are displayed$")
 public void sport_Types_are_displayed() throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 	System.out.println("Sport Types are displayed");
+	Assert.assertTrue("Failed", true);
 }
 
 @When("^user click on a sport \"([^\"]*)\"$")
 public void user_click_on_a_sport(String arg1) throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 	System.out.println("user click on a sport");
+	//Assert.assertTrue("Failed", true);
 }
 
 @Then("^Sports page \"([^\"]*)\" is displayed$")
@@ -32,6 +38,7 @@ public void sports_page_is_displayed(String arg1) throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 	System.out.println("Sports page");
 	//assertTrue("Failing",false);
+	Assert.assertTrue("Failed", true);
 }
 
 
